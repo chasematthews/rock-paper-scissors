a1e10676f9c39f5854c4dd77f27e32031b8f6099
Chase Matthews <chase.matt@outlook.com>
2023-01-07 13:37:04 +1100

    Completed rock-paper-scissors first assignment (slightly cleaned up from previous commit)

--- rps.js
+++ rps.js
@@ -12,19 +12,14 @@ function getComputerChoice() {
 function playRound() {
     getComputerChoice();
     
-    if ((playerSelection == "ROCK" && computerSelection == "ROCK") || (playerSelection == "SCISSORS" && computerSelection == "SCISSORS") || (playerSelection == "PAPER" && computerSelection == "PAPER")
-     ) {        
-        output = `The results is a draw. You both selected ${playerSelection.toLowerCase()}. The score remains Player: ${playerScore}, Computer: ${computerScore}`;
-    }
-    else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || (playerSelection == "SCISSORS" && computerSelection == "PAPER") || (playerSelection == "PAPER" && computerSelection == "ROCK") 
-    ) {
+    if ((playerSelection == "ROCK" && computerSelection == "ROCK") || (playerSelection == "SCISSORS" && computerSelection == "SCISSORS") || (playerSelection == "PAPER" && computerSelection == "PAPER")) {        
+        return `The results is a draw. You both selected ${playerSelection.toLowerCase()}. The score remains Player: ${playerScore}, Computer: ${computerScore}`;
+    } else if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || (playerSelection == "SCISSORS" && computerSelection == "PAPER") || (playerSelection == "PAPER" && computerSelection == "ROCK")) {
         playerScore++;
-        output = `The player wins! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}. The score is now Player: ${playerScore}, Computer: ${computerScore}`;
-    }
-    else if ((playerSelection == "ROCK" && computerSelection == "PAPER") || (playerSelection == "SCISSORS" && computerSelection == "ROCK") || (playerSelection == "PAPER" && computerSelection == "SCISSORS")
-    ) {
+        return `The player wins! ${playerSelection.toLowerCase()} beats ${computerSelection.toLowerCase()}. The score is now Player: ${playerScore}, Computer: ${computerScore}`;
+    } else if ((playerSelection == "ROCK" && computerSelection == "PAPER") || (playerSelection == "SCISSORS" && computerSelection == "ROCK") || (playerSelection == "PAPER" && computerSelection == "SCISSORS")) {
         computerScore++;
-        output = `The computer wins! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}. The score is now Player: ${playerScore}, Computer: ${computerScore}`;
+        return `The computer wins! ${computerSelection.toLowerCase()} beats ${playerSelection.toLowerCase()}. The score is now Player: ${playerScore}, Computer: ${computerScore}`;
     }
 }
 
@@ -32,11 +27,16 @@ function playGame() {
     for (let i = 0; i < 5; i++) {
         playerSelection = prompt("Please choose between rock, paper or scissors").toUpperCase();
         getComputerChoice();
-        playRound();
-        console.log(playerSelection);
-        console.log(computerSelection);
-        console.log(output);
+        console.log(playRound());
     }
+    if (playerScore > computerScore) {
+        return `The player wins with a score of ${playerScore} to ${computerScore}`
+    } else if (playerScore < computerScore) {
+        return `The computer wins with a score of ${computerScore} to ${playerScore}`
+    } else {
+        return `The computer and player draw at a score of ${computerScore}`
+    }
+
 }
 
-playGame();
+console.log(playGame());
